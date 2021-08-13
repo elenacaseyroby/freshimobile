638223e055bb4cb872113e89244dd8bbc15b4e61
Casey Roby <elenacaseyroby@gmail.com>
2021-08-13 19:06:03 -0400

    clean up

--- Freshi/State/Actions/GetPrivacyPolicyAction.swift
+++ Freshi/State/Actions/GetPrivacyPolicyAction.swift
@@ -29,20 +29,20 @@ func getPrivacyPolicyAction(
             if let requestError = requestError {
                 // If request failed return error.
                 onError(requestError)
-                print("FAIL")
                 print(requestError.statusCode)
                 print(requestError.errorMessage ?? "")
             }
             else {
                 // If success, log user in by setting auth creds in cache and updating authStore.isLoggedIn state.
                 if let data = data {
-                    if let pp = try? JSONDecoder().decode(PPResponse.self, from: data) {
+                    if let ppResponse = try? JSONDecoder().decode(PPResponse.self, from: data) {
+                        let pp = ppResponse.privacy_policy
                         // set pp in state
                         DispatchQueue.main.async {
-                            privacyPolicyStore.body = pp.privacy_policy.body
+                            privacyPolicyStore.body = pp.body
                             let dateFormatter: DateFormatter = DateFormatter()
                             dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss:SSS"
-                            privacyPolicyStore.datePublished = dateFormatter.date(from: pp.privacy_policy.date_published) ?? Date()                        }
+                            privacyPolicyStore.datePublished = dateFormatter.date(from: pp.date_published) ?? Date()                        }
                     }
                 }
             }
