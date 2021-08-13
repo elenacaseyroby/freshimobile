0446010cc45b1e6e880f6b6c4e24038776abeed6
Casey Roby <elenacaseyroby@gmail.com>
2021-08-13 18:57:51 -0400

    clean up

--- Freshi/State/Actions/CreateUserAction.swift
+++ Freshi/State/Actions/CreateUserAction.swift
@@ -35,9 +35,6 @@ func createUserAction(
         var email: String
         var first_name: String?
         var last_name: String?
-    //    #if DEBUG
-    //    static let example = MenuItem(id: UUID(), name: "Maple French Toast", photoCredit: "Joseph Gonzalez", price: 6, restrictions: ["G", "V"], description: "Sweet, fluffy, and served piping hot, our French toast is flown in fresh every day from Maple City, Canada, which is where all maple syrup in the world comes from. And if you believe that, we have some land to sell you…")
-    //    #endif
     }
     FreshiRequest(
         endpoint: "users/new/",
